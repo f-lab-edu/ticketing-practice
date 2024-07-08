527fba00bb7bfa57baf2df819c87d500a3c099bc
agi1004 <39743375+agi1004@users.noreply.github.com>
2024-07-08 17:44:33 +0900

    엔티티 생성, 회원 가입, 로그인

--- ticketing-practice/src/main/java/com/ticketingberry/repository/ImageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ticketingberry.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ticketingberry.model.entity.Image;
-
-public interface ImageRepository extends JpaRepository<Image, Long> {
-
-}
