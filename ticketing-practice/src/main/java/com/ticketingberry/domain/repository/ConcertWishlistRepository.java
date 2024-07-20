f59853d8c5c0ac16e58cc7747b0f51dceec5e564
agi1004 <agi1009@naver.com>
2024-07-21 04:43:26 +0900

    제네릭 인터페이스를 확장하도록 변경

--- ticketing-practice/src/main/java/com/ticketingberry/domain/repository/ConcertWishlistRepository.java
+++ ticketing-practice/src/main/java/com/ticketingberry/domain/repository/ConcertWishlistRepository.java
@@ -7,8 +7,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.ticketingberry.domain.entity.Concert;
 import com.ticketingberry.domain.entity.ConcertWishlist;
 import com.ticketingberry.domain.entity.User;
+import com.ticketingberry.domain.repository.common.UserRelatedRepository;
 
-public interface ConcertWishlistRepository extends JpaRepository<ConcertWishlist, Long>{
+public interface ConcertWishlistRepository extends JpaRepository<ConcertWishlist, Long>,
+												   UserRelatedRepository<ConcertWishlist> {
 	List<ConcertWishlist> findByUser(User user);			// 한 명의 회원이 소유한 공연 찜 리스트 찾기
 	List<ConcertWishlist> findByConcert(Concert concert);	// 한 개의 공연을 찜해놓은 공연 찜 리스트 찾기
 }
