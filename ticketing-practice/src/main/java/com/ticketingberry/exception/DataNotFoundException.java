87b65c88c18e364c777f27930f7360be0d5f6bdc
agi1004 <agi1009@naver.com>
2024-07-21 05:18:37 +0900

    @ResponseStatus 추가

--- ticketing-practice/src/main/java/com/ticketingberry/exception/DataNotFoundException.java
+++ ticketing-practice/src/main/java/com/ticketingberry/exception/DataNotFoundException.java
@@ -1,5 +1,9 @@
 package com.ticketingberry.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)	// 404 상태코드
 public class DataNotFoundException extends RuntimeException {
 	public DataNotFoundException(String message) {
 		super(message);
