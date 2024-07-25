2fa39abc51116367943d611b6c9963dcb037f50a
agi1004 <agi1009@naver.com>
2024-07-25 20:08:38 +0900

    @ResponseStatus 삭제

--- ticketing-practice/src/main/java/com/ticketingberry/exception/PasswordsDoNotMatchException.java
+++ ticketing-practice/src/main/java/com/ticketingberry/exception/PasswordsDoNotMatchException.java
@@ -1,9 +1,6 @@
 package com.ticketingberry.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.BAD_REQUEST)	// 400 상태코드
+// 400(BAD_REQUEST)
 public class PasswordsDoNotMatchException extends RuntimeException {
 	public PasswordsDoNotMatchException(String message) {
 		super(message);
