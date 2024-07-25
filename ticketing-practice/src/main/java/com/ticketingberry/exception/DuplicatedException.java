4708b3dad941ce70c7ba4a4f171d5db8da0700f2
agi1004 <agi1009@naver.com>
2024-07-25 20:08:15 +0900

    @ResponseStatus 삭제

--- ticketing-practice/src/main/java/com/ticketingberry/exception/DuplicatedException.java
+++ ticketing-practice/src/main/java/com/ticketingberry/exception/DuplicatedException.java
@@ -1,9 +1,6 @@
 package com.ticketingberry.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.CONFLICT)	// 409 상태코드
+// 409(CONFLICT)
 public class DuplicatedException extends RuntimeException {
 	public DuplicatedException(String message) {
 		super(message);
