7741c949a75b0eb0ee81775682a56e9fb2a4fbbc
agi1004 <agi1009@naver.com>
2024-07-25 20:02:35 +0900

    @ResponseStatus 추가

--- ticketing-practice/src/main/java/com/ticketingberry/advice/ExceptionAdvice.java
+++ ticketing-practice/src/main/java/com/ticketingberry/advice/ExceptionAdvice.java
@@ -1,7 +1,9 @@
 package com.ticketingberry.advice;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.ticketingberry.exception.DataNotFoundException;
@@ -11,24 +13,24 @@ import com.ticketingberry.exception.PasswordsDoNotMatchException;
 @RestControllerAdvice
 public class ExceptionAdvice {
 	// 400(잘못된 요청): 요청의 구문이 잘못되었다.
-	// 상태 코드는 PasswordsDoNotMatchException 클래스에 @ResponseStatus로 설정
 	@ExceptionHandler(PasswordsDoNotMatchException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ResponseBody
 	public String PasswordsDoNotMatchExceptionHandler(PasswordsDoNotMatchException e) {
 		return e.getMessage();
 	}
 	
 	// 404(찾을 수 없음): 지정한 리소스를 찾을 수 없다.
-	// 상태 코드는 DataNotFoundException 클래스에 @ResponseStatus로 설정
 	@ExceptionHandler(DataNotFoundException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
 	@ResponseBody
 	public String DataNotFoundExceptionHandler(DataNotFoundException e) {
 		return e.getMessage();
 	}
 	
 	// 409(충돌): 서버가 요청을 수행하는 중에 충돌이 발생하였다.
-	// 상태 코드는 DuplicatedException 클래스에 @ResponseStatus로 설정
 	@ExceptionHandler(DuplicatedException.class)
+	@ResponseStatus(HttpStatus.CONFLICT)
 	@ResponseBody
 	public String DuplicatedExceptionHandler(DuplicatedException e) {
 		return e.getMessage();
