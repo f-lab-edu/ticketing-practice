58f06e12fe78690f86e147ffeaad98321ed6e162
agi1004 <agi1009@naver.com>
2024-07-29 08:31:59 +0900

    DuplicatedException 클래스 삭제 후 표준 예외 사용

--- ticketing-practice/src/main/java/com/ticketingberry/exception/DuplicatedException.java
+++
@@ -1,9 +0,0 @@
-package com.ticketingberry.exception;
-
-// 409(CONFLICT)
-public class DuplicatedException extends RuntimeException {
-	public DuplicatedException(String message) {
-		super(message);
-	}
-}
-
