9c75ab163f9bbc84bca49cba208ca105f346e047
agi1004 <agi1009@naver.com>
2024-07-29 08:28:43 +0900

    필요없는 메서드 삭제

--- ticketing-practice/src/main/java/com/ticketingberry/domain/repository/UserRepository.java
+++ ticketing-practice/src/main/java/com/ticketingberry/domain/repository/UserRepository.java
@@ -8,5 +8,4 @@ import com.ticketingberry.domain.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 	Optional<User> findByUsername(String username);		// 한 개의 username에 해당하는 회원 한 명 찾기
-	boolean existsByUsername(String username);			// username에 해당하는 회원이 있는지의 여부
 }
