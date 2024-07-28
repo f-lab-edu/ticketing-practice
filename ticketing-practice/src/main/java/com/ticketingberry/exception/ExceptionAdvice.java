b32d6a98d8ce2cb1e3a8a45d64691417da544d9c
agi1004 <agi1009@naver.com>
2024-07-29 08:39:49 +0900

    커스텀 예외에서 표준 예외를 핸들링하게 변경, 리턴 타입을 예외 메시지와 상태 코드를 갖는 클래스로 변경

--- ticketing-practice/src/main/java/com/ticketingberry/exception/ExceptionAdvice.java
+++ ticketing-practice/src/main/java/com/ticketingberry/exception/ExceptionAdvice.java
@@ -1,39 +1,37 @@
-package com.ticketingberry.advice;
+package com.ticketingberry.exception;
 
+import java.util.NoSuchElementException;
+
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import com.ticketingberry.exception.DataNotFoundException;
-import com.ticketingberry.exception.DuplicatedException;
-import com.ticketingberry.exception.PasswordsDoNotMatchException;
-
 @RestControllerAdvice
 public class ExceptionAdvice {
 	// 400(잘못된 요청): 요청의 구문이 잘못되었다.
-	@ExceptionHandler(PasswordsDoNotMatchException.class)
+	@ExceptionHandler(IllegalArgumentException.class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ResponseBody
-	public String PasswordsDoNotMatchExceptionHandler(PasswordsDoNotMatchException e) {
-		return e.getMessage();
+	public ErrorResponse IllegalArgumentExceptionHandler(IllegalArgumentException e) {
+		return new ErrorResponse(e.getMessage(), HttpStatus.BAD_REQUEST.value());
 	}
 	
 	// 404(찾을 수 없음): 지정한 리소스를 찾을 수 없다.
-	@ExceptionHandler(DataNotFoundException.class)
+	@ExceptionHandler(NoSuchElementException.class)
 	@ResponseStatus(HttpStatus.NOT_FOUND)
 	@ResponseBody
-	public String DataNotFoundExceptionHandler(DataNotFoundException e) {
-		return e.getMessage();
+	public ErrorResponse NoSuchElementExceptionHandler(NoSuchElementException e) {
+		return new ErrorResponse(e.getMessage(), HttpStatus.NOT_FOUND.value());
 	}
 	
 	// 409(충돌): 서버가 요청을 수행하는 중에 충돌이 발생하였다.
-	@ExceptionHandler(DuplicatedException.class)
+	@ExceptionHandler(DataIntegrityViolationException.class)
 	@ResponseStatus(HttpStatus.CONFLICT)
 	@ResponseBody
-	public String DuplicatedExceptionHandler(DuplicatedException e) {
-		return e.getMessage();
+	public ErrorResponse DataIntegrityViolationExceptionHandler(DataIntegrityViolationException e) {
+		return new ErrorResponse(e.getMessage(), HttpStatus.CONFLICT.value());
 	}
 }
-
