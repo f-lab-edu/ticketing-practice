cb1092273a1fb4c7cd7ce05ab966ac31e33c3121
agi1004 <agi1009@naver.com>
2024-07-29 08:24:09 +0900

    메서드명을 findUserById에서 findById로 변경

--- ticketing-practice/src/main/java/com/ticketingberry/service/UserService.java
+++ ticketing-practice/src/main/java/com/ticketingberry/service/UserService.java
@@ -1,7 +1,9 @@
 package com.ticketingberry.service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,9 +25,6 @@ import com.ticketingberry.domain.repository.UserRepository;
 import com.ticketingberry.domain.repository.common.UserRelatedRepository;
 import com.ticketingberry.dto.UserDto;
 import com.ticketingberry.dto.UpdateUserDto;
-import com.ticketingberry.exception.DataNotFoundException;
-import com.ticketingberry.exception.DuplicatedException;
-import com.ticketingberry.exception.PasswordsDoNotMatchException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -46,12 +45,12 @@ public class UserService {
 	public Long createUser(UserDto userDto) {
 		// 비밀번호와 비밀번호 확인이 다를 경우 400(BAD_REQUEST) 던지기
 		if (!userDto.getPassword1().equals(userDto.getPassword2())) {
-			throw new PasswordsDoNotMatchException("비밀번호와 비밀번호 확인이 다릅니다.");
+			throw new IllegalArgumentException("비밀번호와 비밀번호 확인이 다릅니다.");
 		}
 		
 		// username으로 회원 조회가 된 경우 중복 객체이므로 409(CONFLICT) 던지기
 		userRepository.findByUsername(userDto.getUsername()).ifPresent(duplicatedUser -> {
-			throw new DuplicatedException("username: " + userDto.getUsername() + "은 이미 존재하는 회원입니다.");
+			throw new DataIntegrityViolationException("username: " + userDto.getUsername() + "은 이미 존재하는 회원입니다.");
 		});
 		
 		// Dto에서 Entity로 변환
@@ -70,23 +69,23 @@ public class UserService {
 	
 	// userId로 회원 조회
 	@Transactional
-	public User readUserById(Long userId) {
+	public User findById(Long userId) {
 		// userId로 회원 조회가 안 된 경우 404(NOT_FOUND) 던지기
 		return userRepository.findById(userId)
-				.orElseThrow(() -> new DataNotFoundException("회원을 찾을 수 없습니다."));
+				.orElseThrow(() -> new NoSuchElementException("회원을 찾을 수 없습니다."));
 	}
 	
 	// username으로 회원 조회
 	@Transactional
-	public User readUserByUsername(String username) {
+	public User findByUsername(String username) {
 		return userRepository.findByUsername(username)
-				.orElseThrow(() -> new DataNotFoundException("username: " + username + " 회원을 찾을 수 없습니다."));
+				.orElseThrow(() -> new NoSuchElementException("username: " + username + " 회원을 찾을 수 없습니다."));
 	}
 	
 	// 회원 수정
 	@Transactional
 	public void updateUser(Long userId, UpdateUserDto updateUserDto) {
-		User user = readUserById(userId);
+		User user = findById(userId);
 		user.update(updateUserDto.getNickname(), updateUserDto.getEmail(), updateUserDto.getPhone());
 		userRepository.save(user);
 	}
@@ -94,50 +93,50 @@ public class UserService {
 	// 회원 삭제
 	@Transactional
 	public void deleteUser(Long userId) {
-		User user = readUserById(userId);
+		User user = findById(userId);
 		userRepository.delete(user);
 	}
 	
 	// 회원 1명의 엔티티 목록 조회
 	@Transactional
-	public <T> List<T> readEntitiesByUserId(Long userId, UserRelatedRepository<T> repository) {
-		User user = readUserById(userId);
+	public <T> List<T> findEntitiesByUserId(Long userId, UserRelatedRepository<T> repository) {
+		User user = findById(userId);
 		return repository.findByUser(user);
 	}
 	
 	// 회원 1명이 작성한 게시글 목록 조회
 	@Transactional
-	public List<Article> readArticlesByUserId(Long userId) {
-		return readEntitiesByUserId(userId, articleRepository);
+	public List<Article> findArticlesByUserId(Long userId) {
+		return findEntitiesByUserId(userId, articleRepository);
 	}
 	
 	// 회원 1명이 작성한 게시글 댓글 목록 조회
 	@Transactional
-	public List<ArticleComment> readArticleCommentsByUserId(Long userId) {
-		return readEntitiesByUserId(userId, articleCommentRepository);
+	public List<ArticleComment> findArticleCommentsByUserId(Long userId) {
+		return findEntitiesByUserId(userId, articleCommentRepository);
 	}
 	
 	// 회원 1명이 작성한 콘서트 댓글 목록 조회
 	@Transactional
-	public List<ConcertComment> readConcertCommentsByUserId(Long userId) {
-		return readEntitiesByUserId(userId, concertCommentRepository);
+	public List<ConcertComment> findConcertCommentsByUserId(Long userId) {
+		return findEntitiesByUserId(userId, concertCommentRepository);
 	}
 	
 	// 회원 1명의 콘서트 찜 목록 조회
 	@Transactional
-	public List<ConcertWishlist> readConcertWishlistsByUserId(Long userId) {
-		return readEntitiesByUserId(userId, concertWishlistRepository);
+	public List<ConcertWishlist> findConcertWishlistsByUserId(Long userId) {
+		return findEntitiesByUserId(userId, concertWishlistRepository);
 	}
 	
 	// 회원 1명의 아티스트 찜 목록 조회
 	@Transactional
-	public List<ArtistWishlist> readArtistWishlistsByUserId(Long userId) {
-		return readEntitiesByUserId(userId, artistWishlistRepository);
+	public List<ArtistWishlist> findArtistWishlistsByUserId(Long userId) {
+		return findEntitiesByUserId(userId, artistWishlistRepository);
 	}
 	
 	// 회원 1명의 예매 목록 조회
 	@Transactional
-	public List<Reservation> readReservationsByUserId(Long userId) {
-		return readEntitiesByUserId(userId, reservationRepository);
+	public List<Reservation> findReservationsByUserId(Long userId) {
+		return findEntitiesByUserId(userId, reservationRepository);
 	}
 }
