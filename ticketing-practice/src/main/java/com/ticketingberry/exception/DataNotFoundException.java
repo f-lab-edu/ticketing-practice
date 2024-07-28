ed5341bc12ef1433c2795cb3bcfda4ead86a8c30
agi1004 <agi1009@naver.com>
2024-07-29 08:31:28 +0900

    DataNotFoundException 클래스 삭제 후 표준 예외 사용

--- ticketing-practice/src/main/java/com/ticketingberry/exception/DataNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.ticketingberry.exception;
-
-// 404(NOT_FOUND)
-public class DataNotFoundException extends RuntimeException {
-	public DataNotFoundException(String message) {
-		super(message);
-	}
-}
-
