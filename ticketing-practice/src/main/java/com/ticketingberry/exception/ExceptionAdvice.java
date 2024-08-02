83b00674b352cd12f31807159f49fdff2f5968ef
agi1004 <agi1009@naver.com>
2024-08-02 16:15:59 +0900

    표준 예외에서 커스텀 예외로 변경

--- ticketing-practice/src/main/java/com/ticketingberry/exception/ExceptionAdvice.java
+++ ticketing-practice/src/main/java/com/ticketingberry/exception/ExceptionAdvice.java
@@ -1,37 +1,38 @@
 package com.ticketingberry.exception;
 
-import java.util.NoSuchElementException;
-
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.ticketingberry.exception.custom.DataDuplicatedException;
+import com.ticketingberry.exception.custom.DataNotFoundException;
+import com.ticketingberry.exception.custom.PasswordsUnequalException;
+
 @RestControllerAdvice
 public class ExceptionAdvice {
 	// 400(잘못된 요청): 요청의 구문이 잘못되었다.
-	@ExceptionHandler(IllegalArgumentException.class)
+	@ExceptionHandler(PasswordsUnequalException.class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ResponseBody
-	public ErrorResponse IllegalArgumentExceptionHandler(IllegalArgumentException e) {
+	public ErrorResponse PasswordsUnequalExceptionHandler(PasswordsUnequalException e) {
 		return new ErrorResponse(e.getMessage(), HttpStatus.BAD_REQUEST.value());
 	}
 	
 	// 404(찾을 수 없음): 지정한 리소스를 찾을 수 없다.
-	@ExceptionHandler(NoSuchElementException.class)
+	@ExceptionHandler(DataNotFoundException.class)
 	@ResponseStatus(HttpStatus.NOT_FOUND)
 	@ResponseBody
-	public ErrorResponse NoSuchElementExceptionHandler(NoSuchElementException e) {
+	public ErrorResponse DataNotFoundExceptionHandler(DataNotFoundException e) {
 		return new ErrorResponse(e.getMessage(), HttpStatus.NOT_FOUND.value());
 	}
 	
 	// 409(충돌): 서버가 요청을 수행하는 중에 충돌이 발생하였다.
-	@ExceptionHandler(DataIntegrityViolationException.class)
+	@ExceptionHandler(DataDuplicatedException.class)
 	@ResponseStatus(HttpStatus.CONFLICT)
 	@ResponseBody
-	public ErrorResponse DataIntegrityViolationExceptionHandler(DataIntegrityViolationException e) {
+	public ErrorResponse DataDuplicatedExceptionHandler(DataDuplicatedException e) {
 		return new ErrorResponse(e.getMessage(), HttpStatus.CONFLICT.value());
 	}
 }
