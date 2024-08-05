1468ebd8e641498b609b3c69b7f813fc11373f3b
agi1004 <agi1009@naver.com>
2024-08-05 17:56:24 +0900

    [#7] @Setter 제거

--- ticketing-practice/src/main/java/com/ticketingberry/domain/entity/Article.java
+++ ticketing-practice/src/main/java/com/ticketingberry/domain/entity/Article.java
@@ -14,14 +14,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
 @Entity
 @Builder
 @NoArgsConstructor
@@ -30,7 +29,7 @@ import lombok.Setter;
 public class Article {	// 게시글 테이블
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "article_id", nullable = false)
+	@Column(name = "article_id")
 	private long id;		// 게시글 고유 id (1부터 자동 증가)
 	
 	@ManyToOne(fetch = FetchType.LAZY)
@@ -42,17 +41,18 @@ public class Article {	// 게시글 테이블
 	@OneToOne(fetch = FetchType.LAZY)
 	private Image image;		// 게시글에 첨부된 이미지
 	
-	@Column(length = 200, nullable = false)
+	@NotNull
+	@Column(length = 200)
 	private String title;		// 게시글 제목
 	
-	@Column(length = 5000, nullable = false)
+	@NotNull
+	@Column(length = 5000)
 	private String content;		// 게시글 내용
 	
-	@Column(nullable = false)
+	@NotNull
 	private long hits;		// 게시글 조회수 (0부터 증가)
 	
 	@CreationTimestamp
-	@Column(nullable = false)
 	private LocalDateTime createdAt;	// 게시글 객체 생성 시간
 	
 	@LastModifiedDate
