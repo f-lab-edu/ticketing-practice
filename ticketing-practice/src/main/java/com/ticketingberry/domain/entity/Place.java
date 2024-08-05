01d0f83be450fe5acaaafb95a7feab6f3d00d1ec
agi1004 <agi1009@naver.com>
2024-08-05 18:19:59 +0900

    [#7] @Setter 제거, update 메서드 추가

--- ticketing-practice/src/main/java/com/ticketingberry/domain/entity/Place.java
+++ ticketing-practice/src/main/java/com/ticketingberry/domain/entity/Place.java
@@ -11,14 +11,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
 @Entity
 @Builder
 @NoArgsConstructor
@@ -27,16 +26,20 @@ import lombok.Setter;
 public class Place {	// 장소 테이블
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "place_id", nullable = false)
+	@Column(name = "place_id")
 	private long id;	// 장소 고유 id (1부터 자동 증가)
 	
-	@Column(length = 50, nullable = false)
+	@NotNull
+	@Column(length = 50)
 	private String name;	// 장소 이름
 	
 	@CreationTimestamp
-	@Column(nullable = false)
 	private LocalDateTime createdAt;	// 장소 객체 생성 시간
 
 	@LastModifiedDate
 	private LocalDateTime updatedAt;	// 장소 객체 수정 시간
+	
+	public void update(String name) {
+		this.name = name;
+	}
 }
