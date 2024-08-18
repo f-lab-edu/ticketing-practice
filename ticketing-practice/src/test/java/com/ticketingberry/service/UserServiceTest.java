dd04c80c6ddecb91602d8cdef9ca9b1ef7307448
agi1004 <agi1009@naver.com>
2024-08-19 05:34:02 +0900

    [#9] 요청 DTO와 응답 DTO로 타입 수정

--- ticketing-practice/src/test/java/com/ticketingberry/service/UserServiceTest.java
+++ ticketing-practice/src/test/java/com/ticketingberry/service/UserServiceTest.java
@@ -15,10 +15,10 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import com.ticketingberry.domain.entity.*;
-import com.ticketingberry.domain.repository.*;
-import com.ticketingberry.dto.UpdateUserDto;
-import com.ticketingberry.dto.UserDto;
+import com.ticketingberry.domain.user.User;
+import com.ticketingberry.domain.user.UserRepository;
+import com.ticketingberry.dto.user.UpdateUserDto;
+import com.ticketingberry.dto.user.InUserDto;
 import com.ticketingberry.exception.custom.DataDuplicatedException;
 import com.ticketingberry.exception.custom.DataNotFoundException;
 import com.ticketingberry.exception.custom.PasswordsUnequalException;
@@ -32,24 +32,6 @@ public class UserServiceTest {
 	private UserRepository userRepository;
 	
 	@Mock
-	private ArticleRepository articleRepository;
-	
-	@Mock
-	private ArticleCommentRepository articleCommentRepository;
-	
-	@Mock
-	private ConcertCommentRepository concertCommentRepository;
-	
-	@Mock
-	private ConcertWishlistRepository concertWishlistRepository;
-	
-	@Mock
-	private ArtistWishlistRepository artistWishlistRepository;
-	
-	@Mock
-	private ReservationRepository reservationRepository;
-	
-	@Mock
 	private PasswordEncoder passwordEncoder;
 	
 	// 역할: 테스트 대상 객체를 생성하고, 이 객체의 의존성을 자동으로 주입
@@ -60,7 +42,7 @@ public class UserServiceTest {
 	
 	private User user;
 	
-	private UserDto userDto;
+	private InUserDto userDto;
 	
 	@BeforeEach
 	void setUp() {
@@ -69,7 +51,7 @@ public class UserServiceTest {
 				.username("testuser")
 				.build();
 		
-		userDto = UserDto.builder()
+		userDto = InUserDto.builder()
 				.username("testuser")
 				.password1("testpassword")
 				.password2("testpassword")
@@ -95,7 +77,7 @@ public class UserServiceTest {
 	@Test
 	@DisplayName("회원 생성: 비밀번호와 비밀번호 확인이 다르면 PasswordsUnequalException 던지기")
 	void createUser_whenPasswordsDoNotMatch_throwsPasswordsUnequalException() {
-		userDto = UserDto.builder()
+		userDto = InUserDto.builder()
 				.password1("password")
 				.password2("differentPassword")
 				.build();
@@ -189,114 +171,7 @@ public class UserServiceTest {
 	void deleteUser_success() {
 		when(userRepository.findById(1L)).thenReturn(Optional.of(user));
 		User result = userService.delete(1L);
+		verify(userRepository, times(1)).delete(user);
 		assertEquals(user, result);
 	}
-	
-//	@Test
-//	@DisplayName("회원 1명이 작성한 게시글 목록 조회 성공")
-//	void readArticlesByUserId_success() {
-//		Article article1 = Article.builder().user(user).build();
-//		Article article2 = Article.builder().user(user).build();
-//		
-//		List<Article> articles = List.of(article1, article2);
-//		
-//		when(userRepository.findById(1L)).thenReturn(Optional.of(user));
-//		when(articleRepository.findByUser(user)).thenReturn(articles);
-//		
-//		List<Article> result = userService.findArticlesByUserId(1L);
-//		
-//		assertEquals(articles, result);
-//		verify(userRepository, times(1)).findById(1L);
-//		verify(articleRepository, times(1)).findByUser(user);
-//	}
-//	
-//	@Test
-//	@DisplayName("회원 1명이 작성한 게시글 댓글 목록 조회 성공")
-//	void readArticleCommentsByUserId_success() {
-//		ArticleComment articleComment1 = ArticleComment.builder().user(user).build();
-//		ArticleComment articleComment2 = ArticleComment.builder().user(user).build();
-//		
-//		List<ArticleComment> articleComments = List.of(articleComment1, articleComment2);
-//		
-//		when(userRepository.findById(1L)).thenReturn(Optional.of(user));
-//		when(articleCommentRepository.findByUser(user)).thenReturn(articleComments);
-//		
-//		List<ArticleComment> result = userService.findArticleCommentsByUserId(1L);
-//		
-//		assertEquals(articleComments, result);
-//		verify(userRepository, times(1)).findById(1L);
-//		verify(articleCommentRepository, times(1)).findByUser(user);
-//	}
-//	
-//	@Test
-//	@DisplayName("회원 1명이 작성한 콘서트 댓글 목록 조회 성공")
-//	void readConcertCommentsByUserId_success() {
-//		ConcertComment concertComment1 = ConcertComment.builder().user(user).build();
-//		ConcertComment concertComment2 = ConcertComment.builder().user(user).build();
-//		
-//		List<ConcertComment> concertComments = List.of(concertComment1, concertComment2);
-//		
-//		when(userRepository.findById(1L)).thenReturn(Optional.of(user));
-//		when(concertCommentRepository.findByUser(user)).thenReturn(concertComments);
-//		
-//		List<ConcertComment> result = userService.findConcertCommentsByUserId(1L);
-//		
-//		assertEquals(concertComments, result);
-//		verify(userRepository, times(1)).findById(1L);
-//		verify(concertCommentRepository, times(1)).findByUser(user);
-//	}
-//	
-//	@Test
-//	@DisplayName("회원 1명의 콘서트 찜 목록 조회 성공")
-//	void readConcertWishlistsByUserId_success() {
-//		ConcertWishlist concertWishlist1 = ConcertWishlist.builder().user(user).build();
-//		ConcertWishlist concertWishlist2 = ConcertWishlist.builder().user(user).build();
-//		
-//		List<ConcertWishlist> concertWishlists = List.of(concertWishlist1, concertWishlist2);
-//		
-//		when(userRepository.findById(1L)).thenReturn(Optional.of(user));
-//		when(concertWishlistRepository.findByUser(user)).thenReturn(concertWishlists);
-//		
-//		List<ConcertWishlist> result = userService.findConcertWishlistsByUserId(1L);
-//		
-//		assertEquals(concertWishlists, result);
-//		verify(userRepository, times(1)).findById(1L);
-//		verify(concertWishlistRepository, times(1)).findByUser(user);
-//	}
-//	
-//	@Test
-//	@DisplayName("회원 1명의 아티스트 찜 목록 조회 성공")
-//	void readArtistWishlistsByUserId_success() {
-//		ArtistWishlist artistWishlist1 = ArtistWishlist.builder().user(user).build();
-//		ArtistWishlist artistWishlist2 = ArtistWishlist.builder().user(user).build();
-//		
-//		List<ArtistWishlist> artistWishlists = List.of(artistWishlist1, artistWishlist2);
-//		
-//		when(userRepository.findById(1L)).thenReturn(Optional.of(user));
-//		when(artistWishlistRepository.findByUser(user)).thenReturn(artistWishlists);
-//		
-//		List<ArtistWishlist> result = userService.findArtistWishlistsByUserId(1L);
-//		
-//		assertEquals(artistWishlists, result);
-//		verify(userRepository, times(1)).findById(1L);
-//		verify(artistWishlistRepository, times(1)).findByUser(user);
-//	}
-//	
-//	@Test
-//	@DisplayName("회원 1명의 예매 목록 조회 성공")
-//	void readReservationsByUserId_success() {
-//		Reservation reservation1 = Reservation.builder().user(user).build();
-//		Reservation reservation2 = Reservation.builder().user(user).build();
-//		
-//		List<Reservation> reservations = List.of(reservation1, reservation2);
-//		
-//		when(userRepository.findById(1L)).thenReturn(Optional.of(user));
-//		when(reservationRepository.findByUser(user)).thenReturn(reservations);
-//		
-//		List<Reservation> result = userService.findReservationsByUserId(1L);
-//		
-//		assertEquals(reservations, result);
-//		verify(userRepository, times(1)).findById(1L);
-//		verify(reservationRepository, times(1)).findByUser(user);
-//	}
 }
